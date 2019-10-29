f9d270e1f07be3bbe63d6fc1a74dda481b47459c
JerryG09 <phemmy09@gmail.com>
2019-10-29 12:28:08 +0100

    tooling

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  res.send("Got a POST request");
+});
+
+router.put('/', function(req, res, next) {
+  res.send("Got a PUT request");
+});
+
+router.delete('/', function(req, res, next) {
+  res.send("Got a DELETE request");
+});
+
+module.exports = router;
---
+++ src/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Hello User, How are you!');
+});
+router.post('/', function(req, res, next) {
+  res.send('Hello User, What did you send!');
+});
+router.put('/', function(req, res, next) {
+  res.send('Hello User, you made update!');
+});
+router.delete('/', function(req, res, next) {
+  res.send('Hello User, you mean to delete!');
+});
+
+module.exports = router;
