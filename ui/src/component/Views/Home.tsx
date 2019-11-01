29b5c94c65ef7bdb08b9711b0df9d9486e0d35a9
JerryG09 <phemmy09@gmail.com>
2019-11-01 22:15:37 +0100

    ui: Home page

---
+++ ui/src/component/Views/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header, Footer } from '../Layout'
+
+const Home: React.FC<{}> = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export  default Home;
