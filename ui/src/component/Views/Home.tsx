36c80fa22484952a2e9caf3cfcb2dd8a88c5a599
JerryG09 <phemmy09@gmail.com>
2019-11-01 22:24:44 +0100

    added Contact component

--- ui/src/component/Views/Home.tsx
+++ ui/src/component/Views/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header, Footer } from '../Layout'
+import { Contact } from '../Contact'
 
 const Home: React.FC<{}> = () => {
   return (
     <div>
       <Header />
+      <Contact />
       <Footer />
     </div>
   )
