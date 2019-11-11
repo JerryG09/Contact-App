2923a75421904527dd3e20ad679417eb0f5c014e
JerryG09 <phemmy09@gmail.com>
2019-11-11 19:21:45 +0100

    added service page

---
+++ client/src/component/views/Services.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import IndexHeader from '../../common/IndexHeader'
+
+function Services() {
+  return (
+    <>
+      <IndexHeader />
+      <h3>Services...</h3>
+    </>
+  )
+}
+
+export default Services
