e573d0c03c18334ad638d3c51f26561377db5720
JerryG09 <phemmy09@gmail.com>
2019-11-11 19:24:46 +0100

    added FAQ component

---
+++ client/src/component/views/FAQ.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import IndexHeader from '../../common/IndexHeader'
+
+function FAQ() {
+  return (
+    <>
+      <IndexHeader />
+      <h3>FAQ</h3>
+    </>
+  )
+}
+
+export default FAQ
