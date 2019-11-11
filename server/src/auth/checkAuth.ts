3267caa6d427b9a08c78cc3bf3153dcdebbf71cf
JerryG09 <phemmy09@gmail.com>
2019-11-11 19:27:01 +0100

    added Authorization

---
+++ server/src/auth/checkAuth.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import express from 'express';
+
+export const checkAuth = (req: express.Request, res: express.Response,next: express.NextFunction) => {
+  try {
+    const token: string = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, 'secret');
+
+    req.body.userData = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: 'Token Authorization failed',
+    });
+  }
+};
