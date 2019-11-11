30b25c57f75e290c543a1ded965d5b6c3ddc21ab
JerryG09 <phemmy09@gmail.com>
2019-11-11 19:22:38 +0100

    aadded community page

---
+++ client/src/component/views/Community.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import IndexHeader from '../../common/IndexHeader'
+
+function Community() {
+  return (
+    <>
+      <IndexHeader />
+      <h3>Community...</h3>
+    </>
+  )
+}
+
+export default Community
