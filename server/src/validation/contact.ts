b5e8147a6fe46b85b608eb5a8bae92eb6136068f
JerryG09 <phemmy09@gmail.com>
2019-11-14 20:58:06 +0100

    implemented contact validation

--- server/src/validation/contact.ts
+++ server/src/validation/contact.ts
@@ -1,22 +1,54 @@
-import * as Joi from '@hapi/joi';
+import joi from '@hapi/joi';
 
-const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
 
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
 
 const phoneRegex = /^(\+?234[789][01]\d{8})$|^(0[789][01]\d{8})$/;
 
-const contactValidator = Joi.object().keys({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  phone: Joi.string().regex(phoneRegex).required(),
-  email: Joi.string().email().regex(emailRegex).required(),
-  company: Joi.string().min(3).max(30).required()
-})
+// const contactValidator = Joi.object().keys({
+//   name: Joi.string().alphanum().min(3).max(30).required(),
+//   phone: Joi.string().regex(phoneRegex).required(),
+//   email: Joi.string().email().regex(emailRegex).required(),
+//   company: Joi.string().min(3).max(30).required()
+// })
 
-const userValidator = Joi.object().keys({
-  password: Joi.string().regex(passwordRegex).required(),
-  password_confirmation: Joi.any().equal(Joi.ref('password')).required(),
-  email: Joi.string().email().regex(emailRegex)
-})
+// const userValidator = Joi.object().keys({
+//   password: Joi.string().regex(passwordRegex).required(),
+//   password_confirmation: Joi.any().equal(Joi.ref('password')).required(),
+//   email: Joi.string().email().regex(emailRegex)
+// })
 
-export { contactValidator, userValidator }
+const contactSchema = joi.object({
+  firstName: joi
+    .string()
+    .min(1)
+    .max(255)
+    .trim()
+    .lowercase()
+    .required(),
+  lastName: joi
+    .string()
+    .min(1)
+    .max(255)
+    .trim()
+    .lowercase()
+    .required(),
+  email: joi
+    .string()
+    .email()
+    .lowercase()
+    .regex(emailRegex)
+    .allow(''),
+  phone: joi
+    .string()
+    .min(11)
+    .max(14)
+    .pattern(phoneRegex),
+  company: joi
+    .string()
+    .trim()
+    .allow(''),
+});
+
+export { contactSchema }
