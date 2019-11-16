0307cfef1751976a205f7da5594d432a136cddea
JerryG09 <phemmy09@gmail.com>
2019-11-16 03:32:20 +0100

    Modified ContactGriwd component

--- client/src/component/contact/ContactGridView.tsx
+++ client/src/component/contact/ContactGridView.tsx
@@ -4,17 +4,17 @@ import { IoIosContact } from 'react-icons/io';
 function ContactGridView({ contact }: any ) {
   return (
     contact.map((item: any) => (
-      <div className="d-flex px-2 mb-3 border-bottom" style={{ height: '5rem' }}>
-      <div className="">
-        <IoIosContact
-          style={{ fontSize: '4rem' }}
-          className="text-primary pt-2"
-        />
-      </div>
-      <div className="pt-2">
-        <h6 className="text-primary lead">{item.firstName} {item.lastName}</h6>
-        <p className="">{item.email}</p>
-      </div>
+      <div className="d-flex px-2 mb-3 border-bottom" style={{ height: '5rem' }} key={item._id}>
+        <div className="">
+          <IoIosContact
+            style={{ fontSize: '4rem' }}
+            className="text-primary pt-2"
+          />
+        </div>
+        <div className="pt-2">
+          <h6 className="text-primary lead">{item.firstName} {item.lastName}</h6>
+          <p className="">{item.email}</p>
+        </div>
     </div>
     ))
   );
